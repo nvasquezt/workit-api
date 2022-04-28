fa7d4d310d368275d44a5a5c00db91c795e4b36b
Norbey Vasquez <lic.vasquezt@gmail.com>
2022-04-27 20:49:38 -0500

    Query Service fix

--- api/service/index.js
+++ api/service/index.js
@@ -17,7 +17,7 @@ const upload = multer({ dest: './temp' });
 
 
 router.post('/',upload.single('file'),isAuth(),handlerCreateService);
-router.post('/:query',handlerServiceByQuery);
+router.get('/:query',handlerServiceByQuery);
 router.get('/', handlerAllServices);
 router.get('/:id', handlerServiceById);
 router.patch('/:id', isAuth(), handlerUpdateService);
