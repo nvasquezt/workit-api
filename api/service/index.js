2e6ed9b585f88bacbc49e81cfcbb201d81a00c61
Julian Salamanca <jsalamancah@unal.edu.co>
2022-05-03 12:44:29 -0500

    delete temp file

--- api/service/index.js
+++ api/service/index.js
@@ -20,7 +20,7 @@ router.post('/',upload.single('file'),isAuth(),handlerCreateService);
 router.get('/search/:query',handlerSearchServiceById);
 router.get('/', handlerAllServices);
 router.get('/:id', handlerServiceById);
-router.patch('/:id',upload.single('file'), handlerUpdateService);
+router.patch('/:id',upload.single('file'), isAuth() handlerUpdateService);
 router.delete('/:id', isAuth(), handlerDeleteService);
 
 
